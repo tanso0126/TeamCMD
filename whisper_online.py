4c8f9fc733466705764a2348cc8eafc5f2202ced
Watson <dlckdals0126@naver.com>
2025-05-15 18:20:16 +0900

    setting(cuda -> cpu)

--- whisper_online.py
+++ whisper_online.py
@@ -116,7 +116,7 @@ class FasterWhisperASR(ASRBase):
 
 
         # this worked fast and reliably on NVIDIA L40
-        model = WhisperModel(model_size_or_path, device="cuda", compute_type="float16", download_root=cache_dir)
+        model = WhisperModel(model_size_or_path, device="cpu", compute_type="int8", download_root="faster-disk-cache-dir/")
 
         # or run on GPU with INT8
         # tested: the transcripts were different, probably worse than with FP16, and it was slightly (appx 20%) slower
